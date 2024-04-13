a574eb4b3b62bbe41998253019028e5a68867c5e
KaninRaben <97570748+KaninHase@users.noreply.github.com>
2024-04-12 20:42:00 -0600

    Add files via upload

---
+++ texto/Texto.java
@@ -0,0 +1,12 @@
+
+/**
+ * muestra texto
+ */
+public class Texto
+{
+    public static void main (String[] args){
+        System.out.print ("Cuanto es 22 * 10 = ");
+        System.out.print (22*10);
+    }
+}
+
